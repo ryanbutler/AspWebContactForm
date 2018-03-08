00a91a0b9a26537a9d4979dbd7d89067673d6fad
Ryan Butler <ryanbutler@users.noreply.github.com>
2018-03-08 14:23:28 -0600

    adding contact.aspx

---
+++ AspWebContactForm/AspWebContactForm/contact.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="contact.aspx.cs" Inherits="AspWebContactForm.contact" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+</head>
+<body>
+<asp:PlaceHolder ID="formPH" runat="server" Visible="true">
+<form id="form1" runat="server">
+<h3>My Contact Form</h3> 
+<table id="contact" cellspacing="0"> 
+<tr> 
+<td>First Name:</td> 
+<td><asp:TextBox ID="FNameTB" runat="server" /></td> 
+</tr> 
+<tr> 
+<td>Last Name:</td> 
+<td><asp:TextBox ID="LNameTB" runat="server" /></td> 
+</tr> 
+<tr> 
+<td>Email:</td> 
+<td><asp:TextBox ID="EmailTB" runat="server" /></td> 
+</tr> 
+<tr> 
+<td>Comments:</td> 
+<td><asp:TextBox ID="CommentsTB" runat="server" TextMode="MultiLine" /></td> 
+</tr> 
+<tr>
+<td colspan="2"><asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="SendMail"/>
+<asp:Button ID="btnReset" runat="server" Text="Reset" OnClick="Reset"/></td>
+</tr>
+</table> 
+</form>
+</asp:PlaceHolder>
+<asp:PlaceHolder ID="sucessPH" runat="server" Visible="false">
+<p>Thank you for your submission.</p>
+</asp:PlaceHolder>
+
+</body>
+</html>
---
+++ AspWebContactForm/AspWebContactForm/contact.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace AspWebContactForm
+{
+    public partial class contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SendMail(object sender, EventArgs e)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(EmailTB.Text);
+            mail.To.Add("myemail@domain.com");
+            mail.Subject = "Contact Us";
+            mail.IsBodyHtml = false;
+            mail.Body = "First Name: " + FNameTB.Text + "";
+            mail.Body += "Last Name: " + LNameTB.Text + "";
+            mail.Body += "Email: " + EmailTB.Text + "";
+            mail.Body += "Comments: " + CommentsTB.Text + "";
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "your_relay_mail_server";
+            smtp.Send(mail);
+            formPH.Visible = false;
+            sucessPH.Visible = true;
+
+
+        }
+        protected void Reset(object s, EventArgs e)
+        {
+            FNameTB.Text = "";
+            LNameTB.Text = "";
+            EmailTB.Text = "";
+            CommentsTB.Text = "";
+
+        }
+    }
+}
